eb1503b023fa7bb904160a4d43d4f9229c799a46
Shrey <liveprosperdie7@gmail.com>
2026-04-01 16:55:28 +0530

    Add files via upload

---
+++ jar.py
@@ -0,0 +1,41 @@
+class Jar:
+    def __init__(self,capacity=12):
+        self.capacity=capacity
+        self.size=0
+    
+
+    def __str__(self):
+        return self.size*"🍪" 
+
+
+    def deposit(self, n):
+        if self.size+n>self.capacity:
+            raise ValueError
+        self.size+=n
+
+
+    def withdraw(self,n):
+        if self.size<n:
+            raise ValueError
+        self.size-=n
+
+
+    @property    
+    def capacity(self):
+        return self._capacity
+
+
+    @capacity.setter
+    def capacity(self,capacity):
+        if capacity<0:
+            raise ValueError("Invalid Capacity")
+        self._capacity=capacity
+
+
+    @property
+    def size(self):
+        return self._size
+        
+    @size.setter
+    def size(self,size=0):
+        self._size=size
